cb02859ea177adedd1d8b41c37c72b8f8dc276ec
Dhruv Ahire <ahire.dhruv3647@csmu.ac.in>
2026-04-30 15:51:43 +0530

    2

---
+++ Controllers/BuildingController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BuildingController : ControllerBase
+{
+    private readonly BuildingService _service;
+
+    public BuildingController(BuildingService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_service.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var data = _service.GetById(id);
+        if (data == null) return NotFound();
+        return Ok(data);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Building b)
+    {
+        _service.Create(b);
+        return Ok("Created");
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Building b)
+    {
+        _service.Update(id, b);
+        return Ok("Updated");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        _service.Delete(id);
+        return Ok("Deleted");
+    }
+}
---
+++ Services/BuildingService.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Data.SqlClient;
+
+public class BuildingService
+{
+    private readonly DatabaseService _db;
+
+    public BuildingService(DatabaseService db)
+    {
+        _db = db;
+    }
+
+    // GET ALL
+    public List<Building> GetAll()
+    {
+        var list = new List<Building>();
+
+        using (var conn = _db.GetConnection())
+        {
+            conn.Open();
+
+            var cmd = new SqlCommand("SELECT * FROM BUILDING", conn);
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                list.Add(new Building
+                {
+                    BuildingId = (int)reader["building_id"],
+                    BuildingName = reader["building_name"].ToString(),
+                    Location = reader["location"].ToString()
+                });
+            }
+        }
+
+        return list;
+    }
+
+    // GET BY ID
+    public Building? GetById(int id)
+    {
+        using (var conn = _db.GetConnection())
+        {
+            conn.Open();
+
+            var cmd = new SqlCommand(
+                "SELECT * FROM BUILDING WHERE building_id=@id", conn);
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            var reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                return new Building
+                {
+                    BuildingId = (int)reader["building_id"],
+                    BuildingName = reader["building_name"].ToString(),
+                    Location = reader["location"].ToString()
+                };
+            }
+        }
+
+        return null;
+    }
+
+    // CREATE
+    public void Create(Building b)
+    {
+        using (var conn = _db.GetConnection())
+        {
+            conn.Open();
+
+            var cmd = new SqlCommand(
+                "INSERT INTO BUILDING (building_name, location) VALUES (@name,@loc)", conn);
+
+            cmd.Parameters.AddWithValue("@name", b.BuildingName);
+            cmd.Parameters.AddWithValue("@loc", b.Location);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    // UPDATE
+    public void Update(int id, Building b)
+    {
+        using (var conn = _db.GetConnection())
+        {
+            conn.Open();
+
+            var cmd = new SqlCommand(
+                "UPDATE BUILDING SET building_name=@name, location=@loc WHERE building_id=@id", conn);
+
+            cmd.Parameters.AddWithValue("@name", b.BuildingName);
+            cmd.Parameters.AddWithValue("@loc", b.Location);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    // DELETE
+    public void Delete(int id)
+    {
+        using (var conn = _db.GetConnection())
+        {
+            conn.Open();
+
+            var cmd = new SqlCommand(
+                "DELETE FROM BUILDING WHERE building_id=@id", conn);
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
